e21c6b6122964af757aaf210d0eb7bae3135d34b
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-02-29 13:28:59 -0700

    Add select buy item command

--- src/command/SelectBuyItemCommand.java
+++ src/command/SelectBuyItemCommand.java
@@ -4,20 +4,11 @@ import org.osbot.rs07.api.def.ItemDefinition;
 import org.osbot.rs07.api.ui.RS2Widget;
 import utils.GEUtils;
 
-import static utils.ClientUtils.log;
-
-/*
-An alternative is to use the existing getOverallPrice method,
-but its response can be outdated because it pulls from the
-OSBuddy API.
-
-See: https://osbot.org/forum/topic/149620-return-value/?ct=1583006970
- */
-public class CheckPriceCommand extends Command {
+public class SelectBuyItemCommand extends Command {
     private static final String SELECT_ITEM = "Select";
     private ItemDefinition item;
 
-    public CheckPriceCommand(ItemDefinition item) {
+    public SelectBuyItemCommand(ItemDefinition item) {
         this.item = item;
     }
 
